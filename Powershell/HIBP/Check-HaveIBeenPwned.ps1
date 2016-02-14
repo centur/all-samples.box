4bec0432f85bc957ccda8a511e9d6156e1ca902a
Alexey Shcherbak <alexey@drawboard.com>
2016-02-14 23:11:06 +1100

    experimental code cleanup

--- Powershell/HIBP/Check-HaveIBeenPwned.ps1
+++ Powershell/HIBP/Check-HaveIBeenPwned.ps1
@@ -24,33 +24,3 @@ Get-Content -Path $emailsFile |
         }
 
     }
-
-
-#Foreach($Uri in $URLList) {
-#  $error.Clear()
-#
-#  $time = Measure-Command { $request = Invoke-WebRequest -Uri $uri } 2>$null
-#
-#  if ($error.Count -eq 0) {
-#    $time.TotalMilliseconds
-#  } else {
-#    $error[0].Exception.Response
-#    break
-#  }
-#}
-
-#try {$r = Invoke-WebRequest -Uri "$uri/api/4.0/edges" -Body $body -Method:Post -Headers $head -ContentType "application/xml" -TimeoutSec 180 -ErrorAction:Stop} catch {Failure}
-
-
-function Failure {
-    $global:helpme = $body
-    $global:helpmoref = $moref
-    $global:result = $_.Exception.Response.GetResponseStream()
-    $global:reader = New-Object System.IO.StreamReader($global:result)
-    $global:responseBody = $global:reader.ReadToEnd();
-    Write-Host -BackgroundColor:Black -ForegroundColor:Red "Status: A system exception was caught."
-    Write-Host -BackgroundColor:Black -ForegroundColor:Red $global:responsebody
-    Write-Host -BackgroundColor:Black -ForegroundColor:Red "The request body has been saved to `$global:helpme"
-}
-
-
