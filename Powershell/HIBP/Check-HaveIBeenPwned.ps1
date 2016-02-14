5bfed71e407422a93f0f15bef9b33127e2f0ea51
Alexey Shcherbak <alexey@drawboard.com>
2016-02-14 23:18:33 +1100

    fix provided link

--- Powershell/HIBP/Check-HaveIBeenPwned.ps1
+++ Powershell/HIBP/Check-HaveIBeenPwned.ps1
@@ -1,9 +1,7 @@
 cls;
-$emailsFile = "$PSScriptRoot/emails.txt";
-
 $hibpServiceURL = "https://haveibeenpwned.com/api/breachedaccount/";
 
-Get-Content -Path $emailsFile |
+Get-Content -Path "$PSScriptRoot/emails.txt" |
     %{ 
         $r = $null;
         $email = $_;
@@ -13,14 +11,12 @@ Get-Content -Path $emailsFile |
             $r =Invoke-WebRequest -Method:Get -Uri $fullUrl -Header @{ "api-version"= 2} -UserAgent "Centur-powershell-checker" -ContentType "application/json" -TimeoutSec 30 -ErrorAction:Ignore
         }
         catch{
-
             Write-Host "$email - OK!" -ForegroundColor:Green
             #Failure
         }
 
         if($r.StatusCode -eq 200)
         {
-            Write-Host "$_ - Breach DETECTED! See $fullUrl" -BackgroundColor:Red -ForegroundColor:Yellow
+            Write-Host "$_ - Breach DETECTED! See https://haveibeenpwned.com/api/v2/breachedaccount/$email" -BackgroundColor:Red -ForegroundColor:Yellow
         }
-
     }
